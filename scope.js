b01f6eeb4d97e2cbbb59534781563c11f8831375
Jisan-mia <jisanmia47@gmail.com>
2021-06-26 23:54:40 +0600

    js-scopes example added

--- scope.js
+++ scope.js
@@ -1,4 +1,4 @@
-// JavaScript Scopes 
+// JavaScript Scopes
 
 // i) child function can access and modify theire parent variables
 // you define a variable in the window or global scope with name x then you also define a variable with the same name in a child function, it doesn't effect to its parent variables, it exists only in his scopes
@@ -7,7 +7,7 @@
 var x = 20;
 
 function myFunc() {
-	var x = 10;
+	x = 10;
 	console.log(`${x} from myFunc()`);
 }
 
