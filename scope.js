e060843012aeca6933c42c32ad9613d31e36b326
Jisan-mia <jisanmia47@gmail.com>
2021-06-26 23:49:49 +0600

    js-scopes documented

--- scope.js
+++ scope.js
@@ -1,4 +1,4 @@
-// JavaScript Scopes || let vs const
+// JavaScript Scopes 
 
 // i) child function can access and modify theire parent variables
 // you define a variable in the window or global scope with name x then you also define a variable with the same name in a child function, it doesn't effect to its parent variables, it exists only in his scopes
