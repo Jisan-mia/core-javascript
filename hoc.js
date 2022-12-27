38d18a99fb12562e863061e4fe0ef984fa04dbfd
Jisan-mia <jisanmia47@gmail.com>
2022-12-27 13:16:48 +0600

    event loop question, hoc initialized

---
+++ hoc.js
@@ -0,0 +1,8 @@
+// higher order function
+/*
+* functions in javascript are special type of object
+
+- We can assign function to a variable
+- we can pass function as parameter to another function
+- and also we can return a function from a function
+*/
