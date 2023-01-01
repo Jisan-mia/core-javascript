4c7db85533fe10e13239e932a95311d42dfd71f1
Jisan-mia <jisanmia47@gmail.com>
2023-01-01 23:44:31 +0600

    closure exercise 3

--- closures.exercise.js
+++ closures.exercise.js
@@ -13,10 +13,34 @@ answer: Pete
 - the function uses the most recent value of variable name
 */
 
-// example 2: Write function sum that works like this: sum(a)(b) = a+b
+// exercise 2: Write function sum that works like this: sum(a)(b) = a+b
 function sum(a) {
   return function (b) {
     return a+b
   }
 }
 console.log(sum(1)(2)) // 3
+
+
+// exercise 3: array.filter(f) method filters all elements through the function f
+// if it returns true, then the current element returned to the resulting array
+// 1. make an filter function named inBetween(a,b) that returns only elements in between a and b(inclusive)
+// 2. make an filter function named inArray([...]) that returns only elements matching with one of the elements of the original array
+let arr = [1, 2, 3, 4, 5, 6, 7];
+function inBetween(a,b) {
+  return function(currentVal) {
+    return currentVal >= a && currentVal <= b;
+  }
+}
+
+const between2_5 = arr.filter(inBetween(2,5)) // [2,3,4,5]
+console.log(between2_5)
+
+function inArray(arr) {
+  return function (currentVal) {
+    return arr.includes(currentVal)
+  }
+}
+
+const result1 = arr.filter(inArray([4,6,10])) // [4,6]
+console.log(result1)
