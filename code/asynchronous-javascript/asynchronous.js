6282d48875343e4994ebb9a81c84afeab4367f8e
Jisan-mia <jisanmia47@gmail.com>
2023-01-17 16:09:22 +0600

    dynamic typing and primitive types

--- code/asynchronous-javascript/asynchronous.js
+++ code/asynchronous-javascript/asynchronous.js
@@ -1,4 +1,5 @@
 // Asynchronous JavaScript
+// runs multiple code at a time
 /*
 - By nature, JavaScript can only run one operation at a time, it can't run multiple operation at a time
 - for this, javascript is called single-threaded language
---
+++ code/javascript-types/types.js
@@ -0,0 +1,32 @@
+// Types in JavaScript
+
+// Dynamic Typing
+// You don't tell the JavaScript engine what type of data a variable holds, instead the engine figures it out while your code is running
+
+// Variables can hold different types of values because it's all figured out during execution
+
+
+// Primitive Types
+// A primitive type is a type of data that represents a single value or in another word that is, not an object.
+
+// there are 6 primitive types in js
+/*
+i. undefined
+  - undefined represents lack of existence (you shouldn't set a variable to this)
+
+ii. null
+  - null represents lack of existence also but you can set a variable to this
+
+iii. boolean
+  - true or false
+
+iv. number
+  - floating point number (there's always some decimals). Unlike other programming languages, there's only one "number" type... and it can make math weird.
+
+v. string
+  - a sequence of characters inside quote(both '' and "" can be used)
+
+vi. symbol
+  - used in es6 (the next version of JavaScript)
+
+*/
