1a16c497523087a9e31fb52fadcc971b087d915b
Jisan-mia <jisanmia47@gmail.com>
2023-01-18 13:37:02 +0600

    new Object and object literals

---
+++ code/object/object-literals.js
@@ -0,0 +1,18 @@
+// Object Literals
+
+// we can create an object by object literals which is just curly braces
+
+// object literals syntax
+var person = {
+  firstName: 'Jisan',
+  lastName: 'Mia',
+  address: {
+    street: '1025 Main St.',
+    city: 'Dhaka',
+    state: 'Dhaka'
+  }
+}
+
+person.address2 = {
+  street: '333 second st.'
+}
--- code/object/object.js
+++ code/object/object.js
@@ -1,2 +1,25 @@
 // object
-// A collection of name value paris
+// A collection of name value paris sitting in memory, they can contain other name value pairs, that is other objects. they contain other properties, string, booleans, numbers also can contain functions which are called method
+
+// creating object
+var student = new Object();
+
+// [] = computed member access operator has the third highest precedence and is left associativity
+student['firstName'] = 'Jisan';
+student['lastName'] = 'Mia';
+
+var firstNameProperty = 'firstName';
+
+console.log(student);
+console.log(student[firstNameProperty]);
+
+// . = member access operator has the second highest precedence and is left associativity
+console.log(student.lastName)
+
+student.address = new Object()
+student.address.street = '1205 Main St.'
+student.address.city = 'Dhaka'
+student.address.state = 'Dhaka'
+
+console.log(student.address.state);
+console.log(student['address']['street']);
