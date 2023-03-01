27c533c8cabba80f33e4a0233b47e091b5d29827
Jisan-mia <jisanmia47@gmail.com>
2023-03-02 00:37:45 +0600

    asynchronous js summary

---
+++ code/asynchronous-javascript/summary.js
@@ -0,0 +1,7 @@
+// summary of asynchronous javascript
+/*
+- to get the best performance in javascript we should always do heavy calculation in asynchronous way
+- to control order of small task we can use callback function
+- asynchronous task always returns promise, we can execute these promise by nesting then or catch
+- the convinces way to perform asynchronous task, is by using async-await
+*/
