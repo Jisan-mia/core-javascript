1b9a22cbb73a715dcb90af5990c838a735c717d6
Jisan-mia <jisanmia47@gmail.com>
2023-03-01 23:58:19 +0600

    async-await initiated

---
+++ code/asynchronous-javascript/async-await.js
@@ -0,0 +1,34 @@
+// async await in javascript
+const hasMeeting = false;
+
+const meeting = new Promise((resolve, reject) => {
+  if (!hasMeeting) {
+    const meetingDetail = {
+      name: "Technical Discussion",
+      tool: "Google Meet",
+      time: "11:00 AM",
+    };
+    resolve(meetingDetail);
+  } else {
+    reject(new Error("Already meeting scheduled"));
+  }
+});
+
+const addToCalender = (meetingDetail) => {
+  //   return new Promise((resolve) => {
+  //     const calenderDetail = `Meeting for ${meetingDetail.name} added on calender in ${meetingDetail.tool} at ${meetingDetail.time}`;
+  //     resolve(calenderDetail);
+  //   });
+  const calenderDetail = `Meeting for ${meetingDetail.name} added on calender in ${meetingDetail.tool} at ${meetingDetail.time}`;
+  return Promise.resolve(calenderDetail);
+};
+
+// promise chaining
+meeting
+  .then(addToCalender)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
