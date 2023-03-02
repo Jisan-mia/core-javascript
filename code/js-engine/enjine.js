4fa624b05cbdaecf69228f042a97e2ccc6b40055
Jisan-mia <jisanmia47@gmail.com>
2023-03-03 03:57:24 +0600

    js engine

---
+++ code/js-engine/enjine.js
@@ -0,0 +1,10 @@
+// JavaScript Engine
+// a javascript engine is a program or an interpreter which executes javascript code
+// A JavaScript engine can be implemented as a standard interpreter, or just-in-time compiler that compiles JavaScript to bytecode in some form.
+/*
+- A popular example of a JavaScript Engine is Google’s V8 engine. The V8 engine is used inside Chrome and Node.js for example.
+- The engine consists of two main components   
+    i. Memory heap - this is where the memory allocation happens
+    ii. Call Stack - this is where your stack frames are as your code executes
+
+*/
