840def1dbf4ffc37a837bcb8f9aa4e1e7a96900a
Jisan-mia <jisanmia47@gmail.com>
2023-03-06 17:24:54 +0600

    synchronus and asyncrounous code example

--- code/asynchronous-javascript/asynchronous.js
+++ code/asynchronous-javascript/asynchronous.js
@@ -4,6 +4,59 @@
 - By nature, JavaScript can only run one operation at a time, it can't run multiple operation at a time
 - for this, javascript is called single-threaded language
 
-
-
 */
+
+// example of an synchronous code block
+/*
+const processOrder = (customer) => {
+    console.log('processing order for customer 1');
+    var currentTime = new Date().getTime();
+    while(currentTime + 3000 >= new Date().getTime() );
+
+    console.log('order processed for customer 1')
+}
+console.log('take order for customer 1');
+processOrder();
+console.log('completed order for customer 1')
+*/
+
+// example of asynchronous code block
+/*
+const processOrder1 = (customer) => {
+  console.log("processing order for customer 1");
+  setTimeout(() => {
+    console.log("cooking completed");
+  }, 3000);
+
+  console.log("order processed for customer 1");
+};
+console.log("take order for customer 1");
+processOrder1();
+console.log("completed order for customer 1");
+*/
+
+// control the code flow using callback
+const takeOrder = (customer, callback) => {
+  console.log(`take order for ${customer}`);
+  callback(customer)
+};
+
+const processOrderMain = (customer, callback) => {
+  console.log(`processing order for ${customer}`);
+
+  setTimeout(() => {
+    console.log("cooking completed");
+    console.log(`order processed for ${customer}`);
+    callback(customer)
+  }, 3000);
+};
+
+const completeOrder = (customer) => {
+  console.log(`completed order for ${customer}`);
+};
+
+takeOrder('customer 1', (customer) => {
+  processOrderMain(customer, (customer) => {
+    completeOrder(customer)
+  })
+})
