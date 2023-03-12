583e51c57d376be70de29d939c2697ec379203e5
Jisan-mia <jisanmia47@gmail.com>
2023-03-12 12:51:52 +0600

    js good practice

---
+++ code/js-best-practices/index.js
@@ -0,0 +1,21 @@
+// Some javascript best practices
+
+/*
+- Meaningful variable name
+- meaningful and descriptive function name
+- limit the number or function arguments as less as possible
+- use default argument value in function instead of conditionals
+- don't need to pass flag as parameter to function, instead split function for multiple cases
+- its good to create one function for one task or single task per function
+- use strict equality(===) wherever possible
+- don't pollute global prototype
+- use conditional shorthand
+- avoid using eval()
+- add methods on prototype
+- declare variables outside for statement
+- use let/const or variable declaration
+- use semicolon(;)
+- use IIFE 
+- use modular pattern
+
+*/
