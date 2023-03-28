09189bb603db93f83eed8fafcef88793899449d5
Jisan-mia <jisanmia47@gmail.com>
2023-03-28 23:50:57 +0600

    classic closure problem solution: 1

--- code/closures/closures3.js
+++ code/closures/closures3.js
@@ -1,5 +1,5 @@
 // classic closure examples
-var builderFunction = function() {
+function builderFunction() {
     var arr = [];
 
     for(var i = 0; i < 3; i++) {
@@ -16,3 +16,24 @@ var fnLists = builderFunction();
 fnLists[0]() // 3
 fnLists[1]() // 3
 fnLists[2]() // 3
+
+// solving the above issue of logging 3 for each function with iife and closures
+function builderFunction2() {
+    var arr = []
+
+    for(var i = 0; i < 3; i++) {
+        arr.push(
+            (function(j) {
+                return function() {
+                    console.log(j)
+                }
+            }(i))
+        )
+    }
+
+    return arr;
+}
+var fnLists2 = builderFunction2();
+fnLists2[0]() // 0
+fnLists2[1]() // 1
+fnLists2[2]() // 2
