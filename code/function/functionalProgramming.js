76e02a2c3da9bc5f2943330cdd1f68f2ec10ef29
Jisan-mia <jisanmia47@gmail.com>
2023-03-31 22:23:16 +0600

    functional programming examples with bind() method

--- code/function/functionalProgramming.js
+++ code/function/functionalProgramming.js
@@ -9,9 +9,27 @@ function mapForEach(arr, fn) {
     return newArr;
 }
 
-var myArr = [2, 4, 6]
+var myArr = [1, 2, 3]
 
 var doubleIt = mapForEach(myArr, function(currentItem) {
     return currentItem * 2;
 })
-console.log(doubleIt) // [4,8,12]
+console.log(doubleIt) // [2,4,6]
+
+
+// 
+var checkPastLimit = function(limiter, item) {
+    return item > limiter;
+}
+var checkLimitation = mapForEach(myArr, checkPastLimit.bind(this, 1))
+console.log(checkLimitation) // [ false, true, true ]
+
+
+var checkPastLimitSimplified = function(limiter) {
+    return function(limiter, item) {
+        return item > limiter
+    }.bind(this, limiter)
+}
+
+var checkLimitationSimplified = mapForEach(myArr, checkPastLimitSimplified(2))
+console.log(checkLimitationSimplified) //  [ false, false, true ]
