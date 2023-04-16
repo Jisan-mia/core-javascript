1142a45951fb1793a866b9f99b098d94a2fc90a1
Jisan-mia <jisanmia47@gmail.com>
2023-04-17 02:41:50 +0600

    npm module utilize in browser and node

---
+++ code/npm/script-browser.js
@@ -0,0 +1,5 @@
+// npm with browser
+import { v4 as uuid4 } from './node_modules/uuid/dist/esm-browser/index.js';
+
+
+console.log(uuid4())
---
+++ code/npm/script-node.js
@@ -0,0 +1,6 @@
+// npm with node
+const uuid = require('uuid')
+
+const uuid4 = uuid.v4
+
+console.log(uuid4())
