b7b083735bf3662d0ba8a246bcdf1287380cc0d2
Jisan-mia <jisanmia47@gmail.com>
2023-04-19 15:18:16 +0600

    simple parcel implementation

---
+++ code/parcel/script.js
@@ -0,0 +1,7 @@
+import printUserName, { jisan } from "./user.js";
+
+
+import { v4 } from 'uuid';
+
+console.log(printUserName(jisan))
+console.log(v4())
---
+++ code/parcel/user.js
@@ -0,0 +1,11 @@
+export const jisan = {
+    name: 'jisan'
+}
+
+export const sakib = {
+    name: 'sakib'
+}
+
+export default function printUserName(user) {
+    return user.name
+}
