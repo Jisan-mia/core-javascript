732d4cc52693b76cbf06f59af2d10b7f6caa81e9
Jisan-mia <jisanmia47@gmail.com>
2023-05-10 00:12:53 +0600

    completed the cli project(simple atm)

--- code/1-atm-cli-project/script.js
+++ code/1-atm-cli-project/script.js
@@ -3,57 +3,66 @@
 // 1. ask for account
 // 2. if account does not exist, ask to create account
 // 3. ask what user want
-// 4. execute command 
-    // i. view
-    // ii. withdraw
-    // iii. deposit
+// 4. execute command
+// i. view
+// ii. withdraw
+// iii. deposit
 
 // Account class
-const Account = require('./Account.js')
-const CommandLine= require('./CommandLine.js')
+const Account = require("./Account.js");
+const CommandLine = require("./CommandLine.js");
 
 async function main() {
-    const accountName = await CommandLine.ask('What is your account name?')
-    let account = await Account.find(accountName)
+  try {
+    const accountName = await CommandLine.ask("What is your account name?");
+    let account = await Account.find(accountName);
 
-    if(account == null) {
-        account = await promptCreateAccount(accountName)
+    if (account == null) {
+      account = await promptCreateAccount(accountName);
     }
 
-    if(account != null) {
-        await promptWhatUserWant(account)
+    if (account != null) {
+      await promptWhatUserWant(account);
     }
+  } catch (e) {
+    CommandLine.print("Error: please try again!");
+  }
 }
 
 async function promptCreateAccount(accountName) {
-    const res = await CommandLine.ask("That account doesn't exist. Would you like to create it? (yes/no)")   
-    
-    if(res === 'yes') {
-        return await Account.create(accountName)
-    } 
+  const res = await CommandLine.ask(
+    "That account doesn't exist. Would you like to create it? (yes/no)"
+  );
+
+  if (res === "yes") {
+    return await Account.create(accountName);
+  }
 }
 
-
 async function promptWhatUserWant(account) {
-    const res = await CommandLine.ask("What'd you like to do? (view/deposit/withdraw)");
+  const res = await CommandLine.ask(
+    "What'd you like to do? (view/deposit/withdraw)"
+  );
 
-    if(res === 'deposit') {
-        const amount = parseFloat(await CommandLine.ask('How much?'))
-        await account.deposit(amount)
-        CommandLine.print(`Your new balance is: ${account.balance}`)
+  if (res === "deposit") {
+    const amount = parseFloat(await CommandLine.ask("How much?"));
+    await account.deposit(amount);
+    CommandLine.print(`Your new balance is: ${account.balance}`);
+  } else if (res === "withdraw") {
+    const amount = parseFloat(await CommandLine.ask("How much?"));
+    try {
+      await account.withdraw(amount);
+    } catch (e) {
+      CommandLine.print(
+        `Unable to withdraw! Please ensure you have enough balance in your account.`
+      );
     }
+    CommandLine.print(`Your balance is: ${account.balance}`);
+  } else {
+    CommandLine.print(
+      `Your account name: ${account.name} \nYour account balance: ${account.balance}`
+    );
+  }
+}
 
-    else if(res === 'withdraw') {
-        const amount = parseFloat(await CommandLine.ask('How much?'))
-        await account.withdraw(amount)
-        CommandLine.print(`Your new balance is: ${account.balance}`)
-    }
-
-    else if(res === 'view') {
-        CommandLine.print(`Your account name: ${account.name} \nYour account balance: ${account.balance}`)
-    }
-
-
-} 
-
-main()
+main();
