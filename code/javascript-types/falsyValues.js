c9f369ae6d5432c6d0ea79d4939facdd248e1900
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-07-28 23:51:56 +0600

    falsy values

---
+++ code/javascript-types/falsyValues.js
@@ -0,0 +1,13 @@
+// there are 6 falsy values in javascript
+
+/* 
+1. null
+2. undefined
+3. 0
+4. '' (black or empty string)
+5. false
+6. NaN (Not a Number)
+
+- 
+other than these values everything is truthy
+*/
